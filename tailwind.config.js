9ff33e8b948283d274660cb8496cd2e8a45de519
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-10-23 08:09:28 +0900

    added tailwind-css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // 追記
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
