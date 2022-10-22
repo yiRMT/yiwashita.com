628aef7c84b42099249ab2509c119c3cbfd8a817
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-10-23 08:09:28 +0900

    added tailwind-css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+// pages/_app.tsx
+// Tailwind CSS 以外のスタイルファイルはグローバルで適用したくないため削除
+// import '../styles/globals.css'
+// 追記
+import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
