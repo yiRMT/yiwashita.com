c141122a5c5f6fb77d7d8a79fdde613c518ce99e
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-10-24 09:35:19 +0900

    implemented header and footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,34 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="text-gray-600 body-font bg-blue-500">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link href={'/'} passHref>
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl text-white">yiRMT</span>
-          </a>
-        </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 text-white hover:text-gray-900">Profile</a>
-        </nav>
-      </div>
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <div className="flex items-center">
+            <Link href={'/'}>
+              <a className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">yiRMT</a>
+            </Link>
+          </div>
+          <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-2" id="mobile-menu-2">
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <Link href={'/'}>
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href={'/blog/'}>
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                    Blog
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,16 @@
-import type { InferGetStaticPropsType, NextPage } from 'next'
 import { client } from '../libs/client'
-import Link from 'next/link'
 import type { Blog, Tag } from '../types/blog'
-import Image from 'next/image'
+import Head from 'next/head'
 
-export const getStaticProps = async () => {
-  const blog = await client.get({ endpoint: 'blog' });
-  const tag = await client.get({ endpoint: 'tag' });
-
-  return {
-    props: {
-      blogs: blog.contents,
-      tags: tag.contents,
-    },
-  };
-};
-
-type Props = {
-  blogs: Blog[];
-  tags: Tag[];
-};
-
-export default function Home ({blogs, tags}: Props) {
+export default function Home () {
   return (
     <>
+      <Head>
+        <title>Yuichiro Iwashita</title>
+      </Head>
       <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
-        記事一覧
+        トップページ
       </h1>
-      <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-        {blogs.map(blog => (
-          <div className="rounded overflow-hidden shadow-lg" key={blog.id}>
-            <div className="w-full">
-              <Image src={blog.image.url} alt={blog.title} width={blog.image.width} height={blog.image.height} />
-            </div>
-            <div className="px-6 py-4">
-              <Link href={`/blog/${blog.id}`} passHref>
-                <a>{blog.title}</a>
-              </Link>
-            </div>
-            <div className="px-6 py-4">{blog.revisedAt}</div>
-            <div className="px-6 pt-4 pb-2">
-              {blog.tags.map(tag => (
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" key={tag.id}>
-                  #{tag.tag}
-                </span>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
     </>
   )
 }
